a2db33f34960d77b56f6697f98c7cacd9d41bc35
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-04-21 00:53:36 +0530

    Time: 0 ms (100%), Space: 54 MB (48.61%) - LeetHub

---
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumDifference = function(nums) {
+    let min = nums[0];
+    let ans = -1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > min) {
+            ans = Math.max(ans, nums[i] - min);
+        } else {
+            min = nums[i];
+        }
+    }
+
+    return ans;
+};
