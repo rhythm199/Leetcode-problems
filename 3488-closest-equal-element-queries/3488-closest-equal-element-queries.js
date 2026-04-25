4e5dd0575267d47818cc24fcd04f77f46fb61dd5
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-04-25 22:53:17 +0530

    Time: 183 ms (77.94%), Space: 101.6 MB (71.69%) - LeetHub

--- 3488-closest-equal-element-queries/3488-closest-equal-element-queries.js
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.js
@@ -4,5 +4,32 @@
  * @return {number[]}
  */
 var solveQueries = function(nums, queries) {
-    
+     const n = nums.length;
+    const map = new Map();
+
+    for (let i = 0; i < n; i++) {
+        if (!map.has(nums[i])) map.set(nums[i], []);
+        map.get(nums[i]).push(i);
+    }
+
+    const minDist = new Array(n).fill(Infinity);
+
+    for (let indices of map.values()) {
+        if (indices.length === 1) continue;
+
+        let k = indices.length;
+
+        for (let i = 0; i < k; i++) {
+            let curr = indices[i];
+            let next = indices[(i + 1) % k];
+            let prev = indices[(i - 1 + k) % k];
+
+            let distNext = Math.min(Math.abs(curr - next), n - Math.abs(curr - next));
+            let distPrev = Math.min(Math.abs(curr - prev), n - Math.abs(curr - prev));
+
+            minDist[curr] = Math.min(distNext, distPrev);
+        }
+    }
+
+    return queries.map(q => minDist[q] === Infinity ? -1 : minDist[q]);
 };
