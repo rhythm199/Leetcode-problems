a5e3c8f3a0d1df8967f2edbdf7282d77613d4817
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-04-29 15:13:14 +0530

    Time: 1 ms (28.11%), Space: 53.6 MB (62.36%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+       if (!digits) return [];
+
+    const map = {
+        '2': 'abc', '3': 'def', '4': 'ghi',
+        '5': 'jkl', '6': 'mno',
+        '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
+    };
+
+    const res = [];
+
+    function backtrack(index, path) {
+        if (index === digits.length) {
+            res.push(path);
+            return;
+        }
+
+        const letters = map[digits[index]];
+        for (let ch of letters) {
+            backtrack(index + 1, path + ch);
+        }
+    }
+
+    backtrack(0, "");
+    return res; 
+};
