bf9e3949d0b5c847ad22565843ad813388f4c7e3
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-04-30 16:36:40 +0530

    Time: 670 ms (0%), Space: 135.2 MB (0%) - LeetHub

--- 3478-choose-k-elements-with-maximum-sum/3478-choose-k-elements-with-maximum-sum.js
+++ 3478-choose-k-elements-with-maximum-sum/3478-choose-k-elements-with-maximum-sum.js
@@ -4,37 +4,107 @@
  * @param {number} k
  * @return {number[]}
  */
-var findMaxSum = function(nums1, nums2, k) {
-    const n = nums1.length;
-    const arr = [];
+var minHeap = () => {
+  const heap = [];
 
-    for (let i = 0; i < n; i++) {
-        arr.push([nums1[i], i]);
+  const queue = (value) => {
+    heap.push(value);
+
+    let currentIndex = heap.length - 1;
+    let parentIndex = Math.floor((currentIndex - 1) / 2);
+
+    while (heap[parentIndex] > value) {
+      let temp = heap[parentIndex];
+      heap[parentIndex] = value;
+      heap[currentIndex] = temp;
+
+      currentIndex = parentIndex;
+      parentIndex = Math.floor((currentIndex - 1) / 2);
     }
-    arr.sort((a, b) => a[0] - b[0]);
-    const res = Array(n).fill(0);
-    const heap = [];
-    let sum = 0;
+  };
 
-    function push(val) {
-        heap.push(val);
-        heap.sort((a, b) => a - b);
-        sum += val;
+  const dequeue = () => {
+    if (heap.length === 0) return undefined;
 
-        if (heap.length > k) {
-            sum -= heap.shift();
-        }
-    }
+    const removed = heap[0];
 
-    let j = 0;
-    for (let i = 0; i < n; i++) {
-        const [val, idx] = arr[i];
-        while (j < n && arr[j][0] < val) {
-            push(nums2[arr[j][1]]);
-            j++;
-        }
-        res[idx] = sum;
+    if (heap.length === 1) {
+      heap.pop();
+      return removed;
     }
 
-    return res;
+    heap[0] = heap.pop();
+    let currentIndex = 0;
+
+    while (true) {
+      let leftIndex = currentIndex * 2 + 1;
+      let rightIndex = currentIndex * 2 + 2;
+      let minIndex = currentIndex;
+
+      if (leftIndex < heap.length && heap[leftIndex] < heap[minIndex]) {
+        minIndex = leftIndex;
+      }
+
+      if (rightIndex < heap.length && heap[rightIndex] < heap[minIndex]) {
+        minIndex = rightIndex;
+      }
+
+      if (minIndex === currentIndex) break;
+
+      let temp = heap[currentIndex];
+      heap[currentIndex] = heap[minIndex];
+      heap[minIndex] = temp;
+
+      currentIndex = minIndex;
+    }
+
+    return removed;
+  };
+
+  const len = () => {
+    return heap.length;
+  };
+  return { queue, dequeue, len };
+};
+
+var findMaxSum = function (nums1, nums2, k) {
+  const heap = minHeap();
+  const pairs = [];
+  const result = [];
+  let sum = 0;
+  let minimalNum = Infinity;
+  const processed = {};
+
+  for (let i = 0; i < nums1.length; i++) {
+    pairs.push([nums1[i], nums2[i], i]);
+
+    minimalNum = Math.min(minimalNum, nums1[i]);
+  }
+
+  pairs.sort((a, b) => b[0] - a[0]);
+
+  for (let i = pairs.length - 1; i >= 0; i--) {
+    const [num, cost, index] = pairs[i];
+
+    if (num === minimalNum) {
+      result[index] = 0;
+    } else if (processed[num]) {
+      result[index] = processed[num];
+    } else {
+      result[index] = sum;
+    }
+
+    if (!processed[num]) {
+      processed[num] = sum;
+    }
+
+    heap.queue(cost);
+    sum += cost;
+
+    if (heap.len() > k) {
+      sum -= heap.dequeue();
+    }
+  }
+
+  return result;
 };
