4768f3e27594ca58499523600716f35640f7c65d
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-03 23:54:13 +0530

    Time: 0 ms (100%), Space: 55.4 MB (14.53%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    let m = obstacleGrid.length, n = obstacleGrid[0].length;
+    let dp = new Array(n).fill(0);
+
+    dp[0] = obstacleGrid[0][0] === 1 ? 0 : 1;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (obstacleGrid[i][j] === 1) {
+                dp[j] = 0;
+            } else if (j > 0) {
+                dp[j] += dp[j - 1];
+            }
+        }
+    }
+
+    return dp[n - 1];
+};
