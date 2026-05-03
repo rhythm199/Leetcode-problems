36bcfa90aaa6418db68b1b5f255a357fb35258cc
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-04 00:01:25 +0530

    Time: 0 ms (100%), Space: 41.6 MB (99.97%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.isEmpty() || haystack.isEmpty()) return -1;
+        for(int i=0;i<=haystack.length()-needle.length();i++) {
+            if(haystack.charAt(i) == needle.charAt(0))
+            {
+                if(haystack.substring(i,i+needle.length()).equals(needle)) return i;
+            }
+        }
+        return -1;
+    }
+}
