585ff2d8e39239a5e13269d3078939616544aea3
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-04 21:19:42 +0530

    Time: 94 ms (11.65%), Space: 127.3 MB (95.36%) - LeetHub

---
+++ 0077-combinations/0077-combinations.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+    const res = [];
+
+    function backtrack(start, path) {
+        if (path.length === k) {
+            res.push([...path]);
+            return;
+        }
+
+        for (let i = start; i <= n - (k - path.length) + 1; i++) {
+            path.push(i);
+            backtrack(i + 1, path);
+            path.pop();
+        }
+    }
+
+    backtrack(1, []);
+    return res;
+};
