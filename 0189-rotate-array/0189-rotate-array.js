8ee2b8d4f463ec9757c83015f5854ca45f5793d9
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-05 10:58:25 +0530

    Time: 5 ms (30.4%), Space: 67.5 MB (20.47%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    const n = nums.length;
+        k = k % n;
+
+            function reverse(start, end) {
+                    while (start < end) {
+                                [nums[start], nums[end]] = [nums[end], nums[start]];
+                                            start++;
+                                                        end--;
+                                                                }
+                                                                    }
+
+                                                                        reverse(0, n - 1);     
+                                                                            reverse(0, k - 1);
+                                                                                reverse(k, n - 1); 
+};
