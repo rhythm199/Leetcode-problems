254d49401c3f45172fedf4bbeea8e7959499b1a9
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-06 13:11:13 +0530

    Time: 46 ms (4.25%), Space: 57 MB (9.75%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(columnTitle) {
+    let result = 0;
+    for (let i = 0; i < columnTitle.length; i++) {
+        const charCode = columnTitle.charCodeAt(i) - 64; 
+        result = result * 26 + charCode;
+    }
+    return result;
+};
