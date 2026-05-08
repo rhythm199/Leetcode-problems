a4030cc4d2428ea4ee46b8d24495b5103faf6bdc
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-08 21:08:41 +0530

    Time: 0 ms (100%), Space: 53.8 MB (76.93%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function(s) {
+    if (!s || s[0] === '0') return 0;
+    
+    let n = s.length;
+    let dp = new Array(n + 1).fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        if (s[i - 1] !== '0') {
+            dp[i] += dp[i - 1];
+        }
+        
+        let twoDigit = parseInt(s.substring(i - 2, i));
+        if (twoDigit >= 10 && twoDigit <= 26) {
+            dp[i] += dp[i - 2];
+        }
+    }
+
+    return dp[n];
+};
