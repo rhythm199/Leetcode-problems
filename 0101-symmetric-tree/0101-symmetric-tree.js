93d22784130e3c15ead80f68dee77dab3ae3e118
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-11 15:23:41 +0530

    Time: 0 ms (100%), Space: 58 MB (17.81%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+     const check = (left, right) => {
+        if (!left && !right) return true;
+        if (!left || !right) return false;
+
+        return (
+            left.val === right.val &&
+            check(left.left, right.right) &&
+            check(left.right, right.left)
+        );
+    };
+
+    return check(root.left, root.right);
+};
