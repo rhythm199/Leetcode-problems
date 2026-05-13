0b65456e3e268583b7b2de985440265dabd7c370
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-13 21:55:58 +0530

    Time: 1 ms (43.4%), Space: 55.3 MB (14.15%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerBreak = function(n) {
+    const dp = new Array(n + 1).fill(0);
+
+    dp[1] = 1;
+
+    for (let i = 2; i <= n; i++) {
+        for (let j = 1; j < i; j++) {
+            dp[i] = Math.max(
+                dp[i],
+                j * (i - j),
+                j * dp[i - j]
+            );
+        }
+    }
+
+    return dp[n];
+};
