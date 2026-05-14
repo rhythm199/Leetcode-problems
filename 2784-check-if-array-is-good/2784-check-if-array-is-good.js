a0c9b367bc498971fbd0b5eb69d1df25c6db56a1
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-14 22:24:23 +0530

    Time: 1 ms (72.92%), Space: 56.2 MB (60.42%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isGood = function(nums) {
+      nums.sort((a, b) => a - b);
+
+    let n = nums.length - 1;
+
+    if (nums[n] !== n || nums[n - 1] !== n) {
+        return false;
+    }
+    for (let i = 0; i < n - 1; i++) {
+        if (nums[i] !== i + 1) {
+            return false;
+        }
+    }
+
+    return true;
+};
