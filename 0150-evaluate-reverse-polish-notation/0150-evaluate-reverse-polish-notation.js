811a54c11b3147c15998f231ad32e13c5b189d8c
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-18 20:49:05 +0530

    Time: 5 ms (70.99%), Space: 59 MB (30.77%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    const stack = [];
+
+    for (let token of tokens) {
+        if (token === '+' || token === '-' || token === '*' || token === '/') {
+            const b = stack.pop();
+            const a = stack.pop();
+
+            if (token === '+') stack.push(a + b);
+            else if (token === '-') stack.push(a - b);
+            else if (token === '*') stack.push(a * b);
+            else stack.push(Math.trunc(a / b));
+        } else {
+            stack.push(Number(token));
+        }
+    }
+
+    return stack.pop();
+};
