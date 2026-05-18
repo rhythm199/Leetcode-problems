14ff1ec7d7c2a2639f9b463ba0e63636fadd59ca
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-18 20:57:58 +0530

    Time: 7 ms (98%), Space: 69 MB (36%) - LeetHub

---
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countSubarrays = function(nums, k) {
+    
+    let left = 0;
+    let sum = 0;
+    let ans = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+
+        sum += nums[right];
+
+        while (sum * (right - left + 1) >= k) {
+            sum -= nums[left];
+            left++;
+        }
+
+        ans += (right - left + 1);
+    }
+
+    return ans;
+};
