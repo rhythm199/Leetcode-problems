f5e4da7ca133ca41c3ced893b07da14a28a8bb6b
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-18 20:48:33 +0530

    Time: 64 ms (22.96%), Space: 55.6 MB (100%) - LeetHub

---
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.js
@@ -0,0 +1,42 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var connect = function(root) {
+     if (!root) return root;
+
+    let curr = root;
+
+    while (curr) {
+        let dummy = new Node(0);
+        let tail = dummy;
+
+        while (curr) {
+            if (curr.left) {
+                tail.next = curr.left;
+                tail = tail.next;
+            }
+
+            if (curr.right) {
+                tail.next = curr.right;
+                tail = tail.next;
+            }
+
+            curr = curr.next;
+        }
+
+        curr = dummy.next;
+    }
+
+    return root;
+};
