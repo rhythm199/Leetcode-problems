fdcbdd61fd3999728ba21de8bfa6f11309752adc
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-19 15:08:46 +0530

    Time: 0 ms (100%), Space: 54.3 MB (17.62%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+     const result = [];
+
+    const dfs = (node) => {
+        if (!node) return;
+
+        dfs(node.left);
+        dfs(node.right);
+
+        result.push(node.val);
+    };
+
+    dfs(root);
+
+    return result;
+};
