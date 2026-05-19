05ad96b71fd537aed977a045c212166d8ee99bd9
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-19 15:10:40 +0530

    Time: 30 ms (37.31%), Space: 58.1 MB (73.63%) - LeetHub

---
+++ 0147-insertion-sort-list/0147-insertion-sort-list.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var insertionSortList = function(head) {
+     let dummy = new ListNode(0);
+
+    while (head) {
+
+        let prev = dummy;
+
+        while (prev.next && prev.next.val < head.val) {
+            prev = prev.next;
+        }
+
+        let nextNode = head.next;
+
+        head.next = prev.next;
+        prev.next = head;
+
+        head = nextNode;
+    }
+
+    return dummy.next;
+};
