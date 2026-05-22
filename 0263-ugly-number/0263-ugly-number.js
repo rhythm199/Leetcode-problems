ebcce89471cf1b490aeed45abf4e16996ba7c1e4
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-22 16:27:30 +0530

    Time: 1 ms (44.86%), Space: 55.5 MB (64.11%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function(n) {
+    if (n <= 0) {
+        return false;
+    }
+    const factors = [2, 3, 5];
+    for (const factor of factors) {
+        while (n % factor === 0) {
+            n /= factor;
+        }
+    }
+    return n === 1;
+};
