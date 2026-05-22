c59c443d1f7bbd41ccdf8fd121780991076cb916
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-22 16:20:10 +0530

    Time: 20 ms (29.76%), Space: 61 MB (10.78%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const map = new Map();
+
+    for (const ch of s) {
+        map.set(ch, (map.get(ch) || 0) + 1);
+    }
+
+    for (const ch of t) {
+
+        if (!map.has(ch)) {
+            return false;
+        }
+
+        map.set(ch, map.get(ch) - 1);
+
+        if (map.get(ch) === 0) {
+            map.delete(ch);
+        }
+    }
+
+    return map.size === 0;
+};
