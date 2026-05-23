6dc6fb532f49924f449d253e9219ceb5a0d3e849
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-23 21:57:30 +0530

    Time: 0 ms (100%), Space: 57.1 MB (13.14%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} logs
+ * @return {number}
+ */
+var minOperations = function(logs) {
+    let depth = 0;
+
+    for (let log of logs) {
+
+        if (log === "../") {
+
+            if (depth > 0) {
+                depth--;
+            }
+
+        } else if (log !== "./") {
+            depth++;
+        }
+    }
+
+    return depth;
+};
