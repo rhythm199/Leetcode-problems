38de04b583201936c7c1a628f39fccc853886c41
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-05-23 20:46:45 +0530

    Time: 0 ms (100%), Space: 53.8 MB (47.08%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function(nums) {
+      let count = 0;
+    let n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > nums[(i + 1) % n]) {
+            count++;
+        }
+
+        if (count > 1) {
+            return false;
+        }
+    }
+
+    return true;
+};
