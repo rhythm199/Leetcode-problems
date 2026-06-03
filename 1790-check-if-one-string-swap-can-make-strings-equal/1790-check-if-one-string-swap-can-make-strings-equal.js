a3bd954731e723d567ca174c854e5e2b8849f9e5
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-06-03 23:29:18 +0530

    Time: 1 ms (35.71%), Space: 53.9 MB (46.43%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var areAlmostEqual = function(s1, s2) {
+
+    const diff = [];
+
+    for (let i = 0; i < s1.length; i++) {
+        if (s1[i] !== s2[i]) {
+            diff.push(i);
+        }
+    }
+
+    if (diff.length === 0) {
+        return true;
+    }
+
+    if (diff.length !== 2) {
+        return false;
+    }
+
+    const [i, j] = diff;
+
+    return (
+        s1[i] === s2[j] &&
+        s1[j] === s2[i]
+    );
+};
