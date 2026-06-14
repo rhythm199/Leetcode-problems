6d739f0a9fed4fe89f8f8c45ed312d48340fe59c
Rhythm Shukla <46573738+rhythm199@users.noreply.github.com>
2026-06-14 11:19:50 +0530

    Time: 64 ms (82.81%), Space: 104.3 MB (39.06%) - LeetHub

---
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var modifiedList = function(nums, head) {
+    const removeSet = new Set(nums);
+
+    const dummy = new ListNode(0);
+    dummy.next = head;
+
+    let prev = dummy;
+    let curr = head;
+
+    while (curr) {
+        if (removeSet.has(curr.val)) {
+            prev.next = curr.next;
+        } else {
+            prev = curr;
+        }
+
+        curr = curr.next;
+    }
+
+    return dummy.next;
+};
